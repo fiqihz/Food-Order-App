33cf02d59f08dda47cc4ca652e74c0f26b971a56
Andi Fiqih Zulfikar Ashari <fiqihz096@gmail.com>
2022-10-24 23:58:41 +0700

    Add Meals Components

---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,16 @@
+const MealsSummary = () => {
+  return (
+    <section className="text-center max-w-3xl w-[90%] m-auto -mt-40 relative bg-[#383838] text-white rounded-2xl p-4 shadow-lg">
+      <h2 className="text-4xl mt-0">Delicious Food, Delivered To You</h2>
+      <p>
+        Choose your favorite meal from our broad selection of available meals
+        and enjoy a delicious lunch or dinner at home.
+      </p>
+      <p>
+        All our meals are cooked with high-quality ingredients, just-in-time and
+        of course by experiences chefs!
+      </p>
+    </section>
+  );
+};
+export default MealsSummary;
